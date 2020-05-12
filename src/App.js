5575078e0b1eb67270083c5d9717abfd234a2bc2
Ian Yang <iany.yang@mail.utoronto.ca>
2020-05-12 04:58:48 -0400

    Implemented file type and file size client-side validation

--- src/App.js
+++ src/App.js
@@ -7,19 +7,65 @@ class App extends Component {
         super(props);
 
         this.state = {
+            maxSize: 5000000,
+            validTypes: ['image/png', 'image/jpeg', 'image/jpg'],
             selectedFile: null,
             loaded: 0
         }
     }
 
-    chooseFileOnChangeHandler = (event) => {
-        console.log(event.target.files[0])
-        this.setState({
-            selectedFile: event.target.files[0],
-            loaded: 0,
-        })
+    // Check if the upload follows proper file size limits
+    checkFileSize = (event) => {
+        let files = event.target.files
+        let err = ''
+
+        for (let x = 0; x < files.length; x++) {
+            if (files[x].size > this.state.maxSize) {
+                err += files[x].type + ' is too large!\n'
+            }
+        };
+
+        if (err !== '') {
+            event.target.value = null
+            console.log(err)
+            return false
+        }
+
+        return true;
     }
 
+    // Check if the upload follows proper file types
+    checkFileType = (event) => {
+        let files = event.target.files
+        let err = ''
+
+        for (let x = 0; x < files.length; x++) {
+            if (this.state.validTypes.every(type => files[x].type !== type)) {
+                err += files[x].type + ' is not a supported format!\n';
+            }
+        };
+
+        if (err !== '') {
+            event.target.value = null
+            console.log(err)
+            return false;
+        }
+
+        return true;
+    }
+
+    // onChange handler for choosing files
+    chooseFileOnChangeHandler = (event) => {
+        var files = event.target.files
+
+        if (this.checkFileSize(event) && this.checkFileType(event)) {
+            this.setState({
+                selectedFile: files[0]
+            })
+        }
+    }
+
+    // onClick handler for upload file button
     uploadFileOnClickHandler = (event) => {
         const data = new FormData()
         data.append('file', this.state.selectedFile)
