ce92d4db24ca71bd389e7049e71480801776db9c
Ian Yang <iany.yang@mail.utoronto.ca>
2020-05-17 22:10:07 -0400

    Completed implementation of all GET requests (all/specific team queries, all/specific uploaded images)

--- backend/server.js
+++ backend/server.js
@@ -40,14 +40,31 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 
-// GET: all previous queries (todo)
-app.get('/', (req, res) => {
-    return res.send('Hello');
+// GET: all previous team queries
+app.get('/teams', (req, res) => {
+    Teams.find()
+        .then(teams => res.json(teams))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
-// GET: specific query (todo)
-app.get('/:id', (req, res) => {
-    return res.send('Hello');
+// GET: specific team query
+app.get('/teams/:id', (req, res) => {
+    Teams.findById(req.params.id)
+        .then(team => res.json(team))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// GET: all previous uploaded images
+app.get('/images', (req, res) => {
+    Image.find()
+        .then(images => res.json(images))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+// GET: specific uploaded image
+app.get('/images/:id', (req, res) => {
+    Image.findById(req.params.id)
+        .then(image => res.json(image))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 // POST: Tesseract.js image results
@@ -142,49 +159,49 @@ app.post('/upload', (req, res) => {
             })
             .catch(err => res.status(400).json('Error: ' + err));
     })
+
+    // Image editing via Jimp
+    async function editImage(img) {
+        const image = await jimp.read(img);
+        image.greyscale().contrast(+0.15).normalize().invert();
+
+        const processed = image.writeAsync('./uploads/processed.png');
+        const composite = image.writeAsync('./uploads/composite.png');
+        await processed;
+        await composite;
+    }
+
+    // Image cropping via Jimp
+    async function cropImage(img, i, x, y, w, h) {
+        const image = await jimp.read(img);
+        image.crop(x, y, w, h);
+
+        await image.writeAsync(`./uploads/crop ${i}.png`);
+    }
+
+    // Image composition via Jimp
+    async function composeImage(img1, img2, x, y) {
+        const image1 = await jimp.read(img1);
+        const image2 = await jimp.read(img2);
+        image1.composite(image2, x, y);
+
+        await image1.writeAsync(img1);
+    }
+
+    // Tesseract.js image processing
+    async function recognizeImage(img, raw) {
+        const worker = createWorker();
+        await worker.load();
+        await worker.loadLanguage('eng');
+        await worker.initialize('eng');
+
+        const { data: { text } } = await worker.recognize(img);
+        raw.push(text);
+
+        await worker.terminate();
+        console.log('Tesseract.js image processing completed');
+    }
 })
-
-// Image editing via Jimp
-async function editImage(img) {
-    const image = await jimp.read(img);
-    image.greyscale().contrast(+0.15).normalize().invert();
-
-    const processed = image.writeAsync('./uploads/processed.png');
-    const composite = image.writeAsync('./uploads/composite.png');
-    await processed;
-    await composite;
-}
-
-// Image cropping via Jimp
-async function cropImage(img, i, x, y, w, h) {
-    const image = await jimp.read(img);
-    image.crop(x, y, w, h);
-
-    await image.writeAsync(`./uploads/crop ${i}.png`);
-}
-
-// Image composition via Jimp
-async function composeImage(img1, img2, x, y) {
-    const image1 = await jimp.read(img1);
-    const image2 = await jimp.read(img2);
-    image1.composite(image2, x, y);
-
-    await image1.writeAsync(img1);
-}
-
-// Tesseract.js image processing
-async function recognizeImage(img, raw) {
-    const worker = createWorker();
-    await worker.load();
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng');
-
-    const { data: { text } } = await worker.recognize(img);
-    raw.push(text);
-
-    await worker.terminate();
-    console.log('Tesseract.js image processing completed');
-}
 
 // Server start
 app.listen(PORT, function () {
